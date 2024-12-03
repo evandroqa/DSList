21e60d9232dded869e49b82d8c11d5c94eb31fc6
Evandro Queiroz Araujo <evandroqa@ipem.to.gov.br>
2024-12-03 12:34:55 -0300

    Consulta SQL no JPA

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -31,5 +32,12 @@ public class GameService {
 		GameDTO dto = new GameDTO(game);
 		return dto;
 	}
-	
+
+	@Transactional(readOnly = true) // Não vai bloquear o banco para escrita
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+
 }
